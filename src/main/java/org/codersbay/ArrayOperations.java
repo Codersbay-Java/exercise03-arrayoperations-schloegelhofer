3de3343da88e246d8d3c8561a5dbdff1a662d32a
Daniela <91525163+schloegelhofer@users.noreply.github.com>
2022-11-03 19:22:27 +0100

    Teil Abgabe

--- src/main/java/org/codersbay/ArrayOperations.java
+++ src/main/java/org/codersbay/ArrayOperations.java
@@ -1,5 +1,9 @@
 package org.codersbay;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Utility class which performs various operations with integer arrays.
  */
@@ -22,7 +26,11 @@ public class ArrayOperations {
      */
 
     public void print() {
-
+        System.out.print("[");
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+        System.out.print("]");
     }
 
     /**
@@ -30,36 +38,57 @@ public class ArrayOperations {
      * @see <a href="sorting algortihms">http://faculty.cs.niu.edu/~hutchins/csci241/sorting.htm</a>
      */
     public int[] sort() {
-        return null;
+        Arrays.sort(numbers);
+        return numbers;
     }
 
     /**
      * @return the sorted array in reverse order
      */
     public int[] revertSort() {
-        return null;
+        int [] result = new int[numbers.length];
+        int j = 0;
+
+        for (int i = numbers.length-1; i >= 0; i--) {
+            result[j++] = numbers[i];
+            System.out.println(result[i]);
+        }
+        return result;
     }
 
     /**
      * @return the unsorted array in reverted order.
      */
     public int[] reverted() {
-        return null;
+        int [] resultSort = new int[unsorted.length];
+        int j = 0;
+
+        for (int i = numbers.length-1; i >= 0; i--) {
+            resultSort[j++] = numbers[i];
+            System.out.println(resultSort[i]);
+        }
+        return resultSort;
     }
 
     /**
      * @param value which should be searched for.
      * @return true if the array contains the value, false otherwise.
      */
-    public boolean contains(int value) {
-        return false;
-    }
+        public boolean contains(int value) {
+            for (int number : numbers) {
+                if (number == value) {
+                    return true;
+                }
+            }
+            return false;
+        }
 
     /**
      * @return the average value of all elements summed up.
      */
     public double average() {
-        return 0.0;
+        numbers
+    return (numbers);
     }
 
     /**
